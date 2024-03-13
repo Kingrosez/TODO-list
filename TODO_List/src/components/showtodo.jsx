dcdef1276712167f88c3bf0695028697dbd3ded1
Kingrosez <157677497+Kingrosez@users.noreply.github.com>
2024-03-13 13:14:48 +0530

    version 2

--- TODO_List/src/components/showtodo.jsx
+++ TODO_List/src/components/showtodo.jsx
@@ -1,18 +1,27 @@
-function ShowTODO() {
+function ShowTODO({ items }) {
   let name = "fairoz";
   let date = "2019-8-31";
+
   return (
-    <div className="container ">
-      <div className="row row-custom">
-        <div className="col-6">{name}</div>
-        <div className="col-4">{date}</div>
-        <div className="col-2">
-          <button type="button" className="btn btn-danger btn-custom">
-            Delete
-          </button>
-        </div>
+    <>
+      <div className="container ">
+        {items.map((item) => (
+          <div className="row row-custom">
+            <div key={name} className="col-6">
+              {item.name}
+            </div>
+            <div key={date} className="col-4">
+              {item.date}
+            </div>
+            <div className="col-2">
+              <button type="button" className="btn btn-danger btn-custom">
+                delete
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 }
 export default ShowTODO;
